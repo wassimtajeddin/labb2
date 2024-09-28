3cd77e46677507160598358f5ce11bc85bb018a7
wassimtajeddin <wassim.tajeddin@gmail.com>
2024-09-28 15:11:11 +0200

    Improve Main class and add opencsv library.

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static List<Product> products = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
+        reader();
+
         while (true) {
             System.out.println();
             menuChoices();
@@ -17,89 +19,69 @@ public class Main {
                 case 1 -> writer();
                 case 2 -> search();
                 case 3 -> sortMenu();
-                case 4 -> System.out.println("Ha det gött!");
+                case 4 -> {
+                    System.out.println("Ha det gött!");
+                    return;
+                }
                 default -> System.out.println("Var god välj ett alternativ!");
             }
 
         }
     }
 
-
     public static void reader() throws IOException {
-
-
-        BufferedReader bufferedReader = new BufferedReader(new FileReader("src/Product List.csv"));
-        bufferedReader.readLine();
-        String currentLine;
-        while ((currentLine = bufferedReader.readLine()) != null) {
-
-            String[] detailed = currentLine.split(",");
-            String category = detailed[0];
-            int barcode = Integer.parseInt(detailed[1]);
-            String name = detailed[2];
-            double price = Double.parseDouble(detailed[3]);
-            products.add(new Product(category, barcode, name, price));
-
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/Product List.csv"))) {
+            bufferedReader.readLine();
+            String currentLine;
+            while ((currentLine = bufferedReader.readLine()) != null) {
+                String[] detailed = currentLine.split(",");
+                String category = detailed[0];
+                int barcode = Integer.parseInt(detailed[1]);
+                String name = detailed[2];
+                double price = Double.parseDouble(detailed[3]);
+                products.add(new Product(category, barcode, name, price));
+            }
         }
     }
 
-
     public static void writer() throws IOException {
+        try (CSVWriter writer = new CSVWriter(new FileWriter("src/Product List.csv", true))) {
+            System.out.println("Lägg till en ny kategori:");
+            String category = scanner.next();
+            System.out.println("Lägg till en streckkod:");
+            int barcode = scanner.nextInt();
+            System.out.println("Lägg till varans namn:");
+            String name = scanner.next();
+            System.out.println("Lägg priset:");
+            double price = scanner.nextDouble();
 
-
-        CSVWriter writer = new CSVWriter(new FileWriter(("src/Product List.csv"), true), ',',
-                CSVWriter.NO_QUOTE_CHARACTER,
-                CSVWriter.DEFAULT_ESCAPE_CHARACTER,
-                CSVWriter.DEFAULT_LINE_END);
-
-        System.out.println("lägg till ett nytt kategori!");
-        String category = scanner.next();
-        System.out.println("lägg till en streckkod!");
-        int barcode = scanner.nextInt();
-        System.out.println("lägg till varansnamn!");
-        String name = scanner.next();
-        System.out.println("lägg priset!");
-        int price = scanner.nextInt();
-        products.add(new Product(category, barcode, name, price));
-        writer.writeNext(new String[]{category, String.valueOf(barcode), name, String.valueOf(price)});
-
-        writer.flush();
-        writer.close();
-
-
+            products.add(new Product(category, barcode, name, price));
+            writer.writeNext(new String[]{category, String.valueOf(barcode), name, String.valueOf(price)});
+        }
     }
 
-    private static void sortByCategory() throws IOException {
-        reader();
+    private static void sortByCategory() {
         products.stream()
                 .sorted(Comparator.comparing(Product::getCategory))
-                .toList()
                 .forEach(System.out::println);
     }
 
-    private static void sortByBarcode() throws IOException {
-        reader();
+    private static void sortByBarcode() {
         products.stream()
                 .sorted(Comparator.comparing(Product::getBarcode))
-                .toList()
                 .forEach(System.out::println);
     }
 
-    private static void sortByName() throws IOException {
-        reader();
+    private static void sortByName() {
         products.stream()
                 .sorted(Comparator.comparing(Product::getName))
-                .toList()
                 .forEach(System.out::println);
     }
 
-    private static void sortByPrice() throws IOException {
-        reader();
+    private static void sortByPrice() {
         products.stream()
                 .sorted(Comparator.comparing(Product::getPrice))
-                .toList()
                 .forEach(System.out::println);
-
     }
 
     private static void menuChoices() {
@@ -112,8 +94,7 @@ public class Main {
                 4. Avsluta""");
     }
 
-
-    private static void sortMenu() throws IOException {
+    private static void sortMenu() {
         System.out.println("""
                 Sortera efter:
                 ======
@@ -121,7 +102,7 @@ public class Main {
                 2. Streckkod
                 3. Namn
                 4. Pris
-                5.Avsluta""");
+                5. Avsluta""");
         int choice = scanner.nextInt();
         switch (choice) {
             case 1 -> sortByCategory();
@@ -133,19 +114,11 @@ public class Main {
         }
     }
 
-    private static void search() throws IOException {
-        System.out.println("Skriv nedan kategoriet:");
+    private static void search() {
+        System.out.println("Skriv nedan kategori:");
         String scannerCategory = scanner.next();
-        String line;
-        FileInputStream inPut = new FileInputStream("src/Product List.csv");
-        Scanner scanner = new Scanner(inPut);
-        while (scanner.hasNextLine()) {
-            line = scanner.nextLine();
-            if (line.startsWith(scannerCategory))
-                System.out.println(line);
-        }
+        products.stream()
+                .filter(product -> product.getCategory().equalsIgnoreCase(scannerCategory))
+                .forEach(System.out::println);
     }
 }
-
-
-
